261893930aa391634b0498d230e700c89d4df35e
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-24 08:47:52 -0600

    creacion de paginacion de producto

--- Kevin20250324.AppWebMVC/Controllers/ProductController.cs
+++ Kevin20250324.AppWebMVC/Controllers/ProductController.cs
@@ -48,8 +48,8 @@ namespace Kevin20250324.AppWebMVC.Controllers
         // GET: Product/Create
         public IActionResult Create()
         {
-            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Id");
-            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Id");
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "Nombre");
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "WarehouseId", "Nombre");
             return View();
         }
 
@@ -66,8 +66,8 @@ namespace Kevin20250324.AppWebMVC.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Id", product.BrandId);
-            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Id", product.WarehouseId);
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "Nombre", product.BrandId);
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "WarehouseId", "Nombre", product.WarehouseId);
             return View(product);
         }
 
@@ -84,8 +84,8 @@ namespace Kevin20250324.AppWebMVC.Controllers
             {
                 return NotFound();
             }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Id", product.BrandId);
-            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Id", product.WarehouseId);
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "Nombre", product.BrandId);
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "WarehouseId", "Nombre", product.WarehouseId);
             return View(product);
         }
 
@@ -121,8 +121,8 @@ namespace Kevin20250324.AppWebMVC.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Id", product.BrandId);
-            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Id", product.WarehouseId);
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "Nombre", product.BrandId);
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "WarehouseId", "Nombre", product.WarehouseId);
             return View(product);
         }
 
