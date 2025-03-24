2def903562a5f9700487197326e93c9f3397abd6
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-24 07:53:44 -0600

    modificacion del archivo program

--- Kevin20250324.AppWebMVC/Models/Test20250324DbContext.cs
+++ Kevin20250324.AppWebMVC/Models/Test20250324DbContext.cs
@@ -21,9 +21,7 @@ public partial class Test20250324DbContext : DbContext
 
     public virtual DbSet<Warehouse> Warehouses { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=M05-CII;Initial Catalog=Test20250324DB;Integrated Security=True;Encrypt=False");
+   
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
