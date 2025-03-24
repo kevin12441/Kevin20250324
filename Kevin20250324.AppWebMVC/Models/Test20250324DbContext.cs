4014e5b0c921c7eec9c0acfafbf44f8924f644d4
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-24 07:51:09 -0600

    migracion de base de datos

---
+++ Kevin20250324.AppWebMVC/Models/Test20250324DbContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kevin20250324.AppWebMVC.Models;
+
+public partial class Test20250324DbContext : DbContext
+{
+    public Test20250324DbContext()
+    {
+    }
+
+    public Test20250324DbContext(DbContextOptions<Test20250324DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Warehouse> Warehouses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=M05-CII;Initial Catalog=Test20250324DB;Integrated Security=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Brands__3214EC0746F37396");
+
+            entity.Property(e => e.BrandName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07849A9413");
+
+            entity.Property(e => e.BrandId).HasColumnName("BrandID");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PurchasePrice).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.WarehouseId).HasColumnName("WarehouseID");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Products)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("FK__Products__BrandI__29572725");
+
+            entity.HasOne(d => d.Warehouse).WithMany(p => p.Products)
+                .HasForeignKey(d => d.WarehouseId)
+                .HasConstraintName("FK__Products__Wareho__286302EC");
+        });
+
+        modelBuilder.Entity<Warehouse>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Warehous__3214EC072CD869D2");
+
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.WarehouseName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Kevin20250324.AppWebMVC/Models/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kevin20250324.AppWebMVC.Models;
+
+public partial class Warehouse
+{
+    public int Id { get; set; }
+
+    public string WarehouseName { get; set; } = null!;
+
+    public string? Notes { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
