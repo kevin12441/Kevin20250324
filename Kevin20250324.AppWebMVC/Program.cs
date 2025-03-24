80146b0229404172644e186c56aa9a78ce2411c7
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-24 08:37:23 -0600

    creacion de controladores y vistas

--- Kevin20250324.AppWebMVC/Program.cs
+++ Kevin20250324.AppWebMVC/Program.cs
@@ -1,7 +1,21 @@
+
+using Kevin20250324.AppWebMVC.Modelos;
+using Kevin20250324.AppWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Kevin20250324.AppWebMVC.Modelos.Test20250324DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+builder.Services.AddDbContext<Kevin20250324.AppWebMVC.Models.Test20250324DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
