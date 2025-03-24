5588f76bb0bcb976c3c234d834ef722361fb4840
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-24 07:46:12 -0600

    Agregar archivos de proyecto.

---
+++ Kevin20250324.AppWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Kevin20250324.AppWebMVC.Models;
+
+namespace Kevin20250324.AppWebMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Kevin20250324.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kevin20250324.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
